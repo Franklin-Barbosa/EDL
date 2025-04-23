f39d6331f47a10dd252a25bd99700f288eb5697a
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2025-04-23 14:42:20 +0000

    feat: adicionando comentários ...

--- FilaSimplesmenteEncadeada/IFila.java
+++ FilaSimplesmenteEncadeada/IFila.java
@@ -1,8 +1,8 @@
 
 public interface IFila {
-	    public abstract void enqueue(Object o);
-	    public abstract Object dequeue();
-	    public Object first();
-	    public abstract int size();
-	    public abstract boolean isEmpty();
+	    public abstract void enqueue(Object o); 	// adiciona um elemento à fila.
+	    public abstract Object dequeue(); 			// remoção de um item da fila.
+	    public Object first(); 						// retornar o primeiro elemento da fila, sem removê-lo.
+	    public abstract int size(); 				// etornar o número de elementos na fila.
+	    public abstract boolean isEmpty(); 			// verifica se a fila está vazia.
 }
