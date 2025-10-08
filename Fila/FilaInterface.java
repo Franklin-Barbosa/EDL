eeb6acca50b2495239e9240b69b0cdd5fa92fc48
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2025-10-08 20:26:51 +0000

    feat: implementando estrutura Fila

---
+++ Fila/FilaInterface.java
@@ -0,0 +1,10 @@
+package Fila;
+
+public interface FilaInterface {
+    public abstract void enqueue(Object elemento);
+    public abstract Object dequeue();
+    public abstract Object first();
+    public abstract int size();
+    public abstract boolean isEmpty();
+    
+}
