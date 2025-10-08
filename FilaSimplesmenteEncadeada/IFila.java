2d07a4b084380b91144ed0688408b0c3358792fa
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2025-10-08 20:36:31 +0000

    feat: removendo implementação de estrutura de deu erros...

--- FilaSimplesmenteEncadeada/IFila.java
+++
@@ -1,8 +0,0 @@
-
-public interface IFila {
-	    public abstract void enqueue(Object o); 	// adiciona um elemento à fila.
-	    public abstract Object dequeue(); 			// remoção de um item da fila.
-	    public Object first(); 						// retornar o primeiro elemento da fila, sem removê-lo.
-	    public abstract int size(); 				// etornar o número de elementos na fila.
-	    public abstract boolean isEmpty(); 			// verifica se a fila está vazia.
-}
