2f5d243fee5fb97b8085c7d18883cd3b973dd8d6
Franklin B. <43481761+Franklin-Barbosa@users.noreply.github.com>
2025-10-18 01:10:09 +0000

    feat: removendo implementação.

--- PilhaRubroNegra/PilhaVaziaExcecao.java
+++
@@ -1,9 +0,0 @@
-package PilhaRubroNegra;
-
-class PilhaVaziaExcecao extends RuntimeException {
-
-    /*Exceção de pilha vazia no método embaixo*/
-    public PilhaVaziaExcecao(String msg) {
-        super(msg);
-    }
-}
